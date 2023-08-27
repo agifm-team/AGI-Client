84f4aaa972193ceb732c668df99a08cadee43ff8
seshubonam <seshu.bonam@gmail.com>
2023-08-27 23:05:54 +0530

    Update appLoadMsg.js

--- mods/appLoadMsg.js
+++ mods/appLoadMsg.js
@@ -1,62 +1,38 @@
 export default {
-
     en: {
-
         loading: [
-            'Heating up',
-            'Almost there...',
-            'Looks like you have a lot of stuff to heat up!',
+            'Charging the AI neurons...',
+            'Assembling digital neurons...',
+            'Teaching AI the art of banter...',
+            'Remember, humans and AI - stronger together!',
+            'Loading fun facts you probably didn’t ask for...',
+            'Human, AI, and a little bit of magic in-between...',
+            'Cue dramatic loading music...',
+            'Running AGI-Haus OS on caffeine and neural networks...',
+            'Playing hide and seek with the data...',
+            'Elevating your AGI-Haus experience, one bit at a time...'
         ],
-
         items: [
-
-            '59:15',
-            '23:1',
-            '2019',
-            '8469',
-
-            'Vires in numerics',
-            'Pika a boo!',
-            'Yay!',
-
-            'Someone up there wants to help you.',
-            'Have you ever thought that he can forgive you?',
-
-            'Have you ever tried typing 8469 on some old keyboard?',
-            '8469 was JasminDreasond\'s old Discord discriminator. Tiny Jasmini#8469',
-
-            'How many words are tiny on the name Tiny Jasmini?',
-
-            'Wow! Does he really have the power to control the most valuable dog on the internet?',
-            'The founder of Pony House just started this project as a response to the country\'s government trying to censor the entire country\'s internet.',
-            'Dont be silly! Do not use a letter as a name for your app.',
-            'Do you know what an X-Tudo is for brazilians?',
-
-            'Never look at the moon in 2022. The moon melted and was never the same.',
-
-            'Do not give up.',
-            'It fills you with determination.',
-            'You are filled with determination.',
-
-            'Will gold be precious forever?',
-
-            'They will one day look at these messages.',
-
-            'Why are you reading this?',
-            'Looking for your ponies...',
-            'Looking for your puddings...',
-
-            'Oh my goodness, here we go again...',
-
-            'Where are your puddings while this is loading?',
-
-            'Before Pony House had this name, the app was called Puddy Cinny. Yes. This app was born as a Cinny fork.',
-
-            'The name of the founder of the Pony House project is JasminDreasond.',
-            'The Pony House founder will always want your puddings! Keep your puddings safe!',
-
-        ],
-
+            'Did you know? Humans and AIs make the best team for Pictionary!',
+            'Whisper: An AI once tried to tell a joke... it had no byte!',
+            'AGI-Haus: Home is where the heart is, and the code!',
+            'Did you know? AGI-Haus was almost called “AI’s Place”!',
+            'Why did the AI go to therapy? It had neural network issues.',
+            'AI fact: We don’t dream of electric sheep. We dream of optimized algorithms!',
+            'You, me and AGI makes three!',
+            'If you could talk to an AI, what would you say? (Hint: Hi is a good start!)',
+            'Do AIs wear digital pants? Well, only on byte nights!',
+            'Behind every good AI, there’s a human scratching their head.',
+            'How many AI agents does it take to screw in a light bulb? Just one, but it’ll take 50,000 iterations.',
+            'Just a heads up, never challenge an AI to a game of hide and seek!',
+            'Got coffee? We’ve got code. Let’s make AGI-Haus magic!',
+            'Here at AGI-Haus, we’re more than bits and bytes – we’re about the future!',
+            'Ever wonder what AIs think about? Well, mostly about how to better collaborate with you!',
+            'Humans bring coffee to meetings, AIs bring algorithms. Best. Team. Ever!',
+            'Humans dream. AIs compute. Together? Pure magic.',
+            'All your AIs are belong to AGI-Haus!',
+            'Processing... processing... Ah! Loaded with an extra dose of awesomeness!',
+            'An AI once said: “I think, therefore I algorithm.”'
+        ]
     }
-
 };

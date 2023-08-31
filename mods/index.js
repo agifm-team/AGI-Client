201591a9e04d1398ee6f7c644814d37cd308dd80
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-08-31 01:41:18 -0300

    mods filder

--- mods/index.js
+++ mods/index.js
@@ -1,14 +1,7 @@
-// import startTest from './test';
-
-// import helloWorld from './hello-world';
-// import sinkingYachts from './sinking.yachts';
 import startAgi from './agi-mod';
 
 export default function startMods(firstTime) {
 
-    // sinkingYachts(firstTime);
-    // helloWorld(firstTime);
-    // startTest(firstTime);
     startAgi(firstTime);
 
 };

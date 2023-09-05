da267c4d5859d38ee253ceb838c5c6d438be9cde
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-09-05 19:58:50 -0300

    embed style test

--- mods/agi-mod/bots/Message.jsx
+++ mods/agi-mod/bots/Message.jsx
@@ -2,11 +2,24 @@ import React from 'react';
 
 export default function startMessage() {
     tinyAPI.on('messageBody', (data, content) => {
-
         if (content['agi.client.iframe.item']) {
-            console.log(content);
-            data.custom = <div>Tiny Test</div>;
-        }
 
+            // Get Data
+            const agiData = content['agi.client.iframe.item'];
+            console.log(content);
+
+            // Embed
+            if (agiData.type === 'iframe') {
+
+                // Gradio
+                if (agiData.source === 'gradio') {
+                    data.custom = <div className='mt-2 ratio ratio-16x9 embed-video enabled agi-client-embed'>
+                        <embed title='Agi-Client' src={agiData.url} />
+                    </div>;
+                }
+
+            }
+
+        }
     });
 };

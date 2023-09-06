c560dcc7da6f1ba3b3c59f23186c35ff55e04601
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-09-06 20:01:34 -0300

    agent button update

---
+++ mods/agi-mod/bots/PeopleSelector/Item.jsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+
+import { selectRoom, selectRoomMode, selectTab } from '../../../../src/client/action/navigation';
+import cons from '../../../../src/client/state/cons';
+
+import * as roomActions from '../../../../src/client/action/room';
+
+import {
+    hasDMWith, hasDevices,
+} from '../../../../src/util/matrixUtil';
+
+function PeopleSelector({ avatarSrc, name, peopleRole, }) {
+
+    // Refs
+    const buttonRef = useRef(null);
+
+    // Effect
+    useEffect(() => {
+
+        // Get Button
+        const button = $(buttonRef.current);
+        const tinyButton = async () => {
+
+            // Select tab and bot id
+            selectTab(cons.tabs.DIRECTS);
+            const userId = button.attr('bot');
+
+            // Check and open if user already have a DM with userId.
+            const dmRoomId = hasDMWith(userId);
+            if (dmRoomId) {
+                selectRoomMode('room');
+                selectRoom(dmRoomId);
+                return;
+            }
+
+            // Create new DM
+            try {
+                $.LoadingOverlay('show');
+                await roomActions.createDM(userId, await hasDevices(userId));
+                $.LoadingOverlay('hide');
+            } catch (err) {
+                console.error(err);
+                alert(err.message);
+            }
+
+        };
+
+        // Insert Event Click
+        button.on('click', tinyButton);
+        return () => {
+            button.off('click', tinyButton);
+        };
+
+    });
+
+    return <div ref={buttonRef} bot={name} className="card agent-button noselect">
+        <div className='text-start my-3 mx-4'><img src={avatarSrc} className="img-fluid avatar rounded-circle" draggable={false} height={100} width={100} alt="avatar" /></div>
+        <div className="text-start card-body mt-0 pt-0">
+            <h5 className="card-title small text-bg">{name}</h5>
+            <p className="card-text very-small text-bg-low">{peopleRole}</p>
+        </div>
+    </div>;
+
+}
+
+PeopleSelector.defaultProps = {
+    avatarAnimSrc: null,
+    avatarSrc: null,
+    peopleRole: null,
+    user: null,
+    disableStatus: false,
+};
+
+PeopleSelector.propTypes = {
+    disableStatus: PropTypes.bool,
+    user: PropTypes.object,
+    avatarAnimSrc: PropTypes.string,
+    avatarSrc: PropTypes.string,
+    name: PropTypes.string.isRequired,
+    color: PropTypes.string.isRequired,
+    peopleRole: PropTypes.string,
+    onClick: PropTypes.func.isRequired,
+};
+
+export default PeopleSelector;
---
+++ mods/agi-mod/bots/PeopleSelector/index.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useRef } from 'react';
+
+import defaultAvatar from '../../../../src/app/atoms/avatar/defaultAvatar';
+import { serverAddress } from '../../socket';
+import PeopleSelector from './Item';
+
+let tinyData = null;
+function updateAgentsList() {
+    return new Promise((resolve) => {
+        fetch(`${serverAddress}api/v1/get_bots/user15`, {
+            headers: {
+                'Accept': 'application/json'
+            }
+        }).then(res => res.json()).then((newData) => {
+            tinyData = newData;
+            resolve();
+        }).catch(err => {
+            console.error(err);
+            alert(err.message);
+            resolve();
+        });
+    });
+};
+
+export default function startPeopleSelector() {
+
+    // Members List
+    updateAgentsList();
+    tinyAPI.on('roomMembersOptions', (data, items) => {
+
+        updateAgentsList();
+
+        if (Array.isArray(tinyData)) {
+            for (const item in tinyData) {
+                if (typeof tinyData[item] === 'string') {
+                    items.unshift({
+                        name: 'Agents', value: 'agents', custom: [
+                            {
+
+                                avatarSrc: defaultAvatar(1),
+                                name: tinyData[item],
+
+                                peopleRole: "Bot",
+                                powerLevel: undefined,
+                                userId: tinyData[item],
+                                username: tinyData[item],
+
+                                customClick: () => { console.log('event test'); },
+                                customSelector: PeopleSelector,
+
+                            }
+                        ]
+                    });
+                }
+            }
+        }
+
+        /* items.unshift({
+            name: 'Agents-Joined', value: 'agents-joined', custom: [
+
+            ]
+        }); */
+
+    });
+
+};

dee7b1bc4524b41442962432e3fd6c741c3d7e20
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-09-05 21:35:09 -0300

    embed test

--- mods/agi-mod/bots/Message/GradioEmbed.jsx
+++ mods/agi-mod/bots/Message/GradioEmbed.jsx
@@ -1,17 +1,40 @@
 import React, { useRef, useEffect } from 'react';
 
+if ($('head').find('#gradio-api').length < 1) {
+    $('head').append($('<script>', { src: 'https://gradio.s3-us-west-2.amazonaws.com/3.42.0/gradio.js', id: 'gradio-api', type: 'module' }))
+}
+
 function GradioEmbed({ agiData }) {
 
     const embedRef = useRef(null);
 
     useEffect(() => {
+        const embed = embedRef.current;
+        if (embedRef.current) {
 
-        const embed = $(embedRef.current);
+            console.log(embedRef.current);
+            const embedMessage = (event) => {
+                console.log('yay! Gradio!');
+                console.log(event);
+            };
 
+            const embedMessage2 = (event) => {
+                console.log('yay! Gradio 2!');
+                console.log(event);
+            };
+
+            embed.addEventListener('render', embedMessage);
+            embed.addEventListener('message', embedMessage2);
+            return () => {
+                embed.removeEventListener('render', embedMessage);
+                embed.removeEventListener('message', embedMessage2);
+            };
+
+        }
     });
 
-    return <div ref={embedRef} className='mt-2 ratio ratio-16x9 embed-video enabled agi-client-embed'>
-        <embed className='webkit-scrollbar-thumb' title='Agi-Client' src={agiData.url} />
+    return <div className='mt-2 ratio ratio-16x9 embed-video enabled agi-client-embed'>
+        <gradio-app ref={embedRef} src={agiData.url} />
     </div>;
 
 };

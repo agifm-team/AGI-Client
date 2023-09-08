e52193562dd17360910aef8d232d5f7111edecce
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-09-08 02:18:00 -0300

    childrenLoader prepare done

--- mods/agi-mod/bots/Message/gradioLayout.js
+++ mods/agi-mod/bots/Message/gradioLayout.js
@@ -1,20 +1,48 @@
 import { objType } from '../../../../src/util/tools';
 
+// Components
+const components = {};
+
 // Children
-const childrenLoader = (item) => {
-    if (objType(item)) {
+const childrenLoader = (items, app) => {
+    if (Array.isArray(items)) {
 
+        // HTML Items
+        const html = [];
 
+        // Read Data
+        for (const item in items) {
+            if (objType(items[item], 'object') && typeof items[item].id === 'number' && !Number.isNaN(items[item].id) && Number.isFinite(items[item].id)) {
+
+                // Page Data
+                let page;
+                if (Array.isArray(items[item].children)) page = childrenLoader(items[item].children);
+
+                // Componet
+                const component = app.components.find(c => c.id === items[item].id);
+                if (objType(component, 'object') && typeof component.type === 'string' && typeof components[component.type] === 'function') {
+                    const tinyHtml = components[component.type](component);
+                    if (page) tinyHtml.append(page);
+                    html.push(tinyHtml);
+                }
+
+            }
+        }
+
+        // Complete
+        return html;
 
     }
 };
 
 export function getHtml(app) {
+    if (objType(app.layout, 'object') && Array.isArray(app.components)) {
 
-    // Get Children
-    const page = childrenLoader(app.layout);
+        // Get Children
+        const page = childrenLoader([app.layout], app);
 
-    // Complete
-    return page;
+        // Complete
+        return page;
 
+    }
 };

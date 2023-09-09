1cc52598615abf1e46b69334e924704c23d7bb21
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-09-08 23:58:22 -0300

    gradio - sanitizeHtml

--- mods/agi-mod/bots/Message/gradioLayout.js
+++ mods/agi-mod/bots/Message/gradioLayout.js
@@ -134,7 +134,17 @@ const components = {
         console.log(`Gallery`, props);
     },
 
-    html: (props) => $(props.value).data('gradio_props', props),
+    html: (props) => $(sanitizeHtml(props.value, {
+        allowedTags: [
+            'address', 'article', 'aside', 'footer', 'header', 'h1', 'h2', 'h3', 'h4',
+            'h5', 'h6', 'hgroup', 'main', 'nav', 'section', 'blockquote', 'dd', 'div',
+            'dl', 'dt', 'figcaption', 'figure', 'hr', 'li', 'main', 'ol', 'p', 'pre',
+            'ul', 'a', 'abbr', 'b', 'bdi', 'bdo', 'br', 'cite', 'code', 'data', 'dfn',
+            'em', 'i', 'kbd', 'mark', 'q', 'rb', 'rp', 'rt', 'rtc', 'ruby', 's', 'samp',
+            'small', 'span', 'strong', 'sub', 'sup', 'time', 'u', 'var', 'wbr', 'caption',
+            'col', 'colgroup', 'table', 'tbody', 'td', 'tfoot', 'th', 'thead', 'tr'
+        ],
+    })).data('gradio_props', props),
 
     highlightedtext: (props) => {
         console.log(`HighlightedText`, props);

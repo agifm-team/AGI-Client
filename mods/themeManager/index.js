7e15f06f0f05c584231105377f4aa5ff1efee723
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-10-20 22:45:38 -0300

    main dark theme name changed

--- mods/themeManager/index.js
+++ mods/themeManager/index.js
@@ -2,9 +2,15 @@ import './theme.scss';
 import tinyAPI from '../../src/util/mods';
 
 export default function startTheme() {
-    tinyAPI.on('loadThemes', (data, insertTheme, removeTheme, getThemeById) => {
+    tinyAPI.on('loadThemes', (data, insertTheme, removeTheme, getThemeById, getThemeNameById) => {
+
         removeTheme('black-theme');
+
         const darkTheme = getThemeById('dark-theme');
         if (darkTheme) darkTheme.type = 'dark-solid';
+
+        const darkThemeName = getThemeNameById('dark-theme');
+        if (darkThemeName) darkThemeName.text = 'Black';
+
     });
 };

88c35136604ba38e57904eab4895ff08f552c700
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-10-20 22:33:38 -0300

    convert dark theme to black theme

--- mods/themeManager/index.js
+++ mods/themeManager/index.js
@@ -2,7 +2,9 @@ import './theme.scss';
 import tinyAPI from '../../src/util/mods';
 
 export default function startTheme() {
-    tinyAPI.on('loadThemes', (data, insertTheme, removeTheme) => {
+    tinyAPI.on('loadThemes', (data, insertTheme, removeTheme, getThemeById) => {
         removeTheme('black-theme');
+        const darkTheme = getThemeById('dark-theme');
+        if (darkTheme) darkTheme.type = 'dark-solid';
     });
 };

aaad1f9a7bf9750798b188b9c30491dcc5b24d7c
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-10-27 13:31:31 -0300

    information added

--- mods/agi-mod/bots/Message/GradioAPI.jsx
+++ mods/agi-mod/bots/Message/GradioAPI.jsx
@@ -89,21 +89,41 @@ function GradioEmbed({ agiData }) {
                                 tinyJson = {};
                             }
 
-                            jsonCache.observe(() => {
+                            jsonCache.observe((event) => {
                                 try {
                                     tinyJson = JSON.parse(jsonCache.toString());
-                                    console.log(tinyJson);
+                                    console.log(event, tinyJson);
                                 } catch {
                                     tinyJson = {};
                                 }
                             });
 
-                            console.log(tinyJson);
+                            console.log(tinyJson, jsonCache, config);
+
+                            /*
+                                Read Data
+
+                                config.components - find by id
+
+                                config.components[id].props
+                                Edit props
+
+
+                            */
 
                             setTimeout(() => {
                                 // tinyJson.yay = 'yay';
                                 // jsonCache.insert(0, JSON.stringify(tinyJson))
                             }, 10000);
+
+                            // array of numbers which produce a sum
+                            const yarray = crdt.ydoc.getArray("count");
+
+                            // observe changes of the sum
+                            yarray.observe(() => {
+                                // print updates when the data changes
+                                console.log(`new sum: ${yarray.toArray().reduce((a, b) => a + b)}`);
+                            });
 
                         }
 

7f021f274fbabed6cebe325d5e17e4d874e0ad1f
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2023-11-24 03:14:27 -0300

    Test - welcome gradio enter room

--- mods/agi-mod/bots/Welcome/item.jsx
+++ mods/agi-mod/bots/Welcome/item.jsx
@@ -9,6 +9,24 @@ import {
     hasDMWith, hasDevices,
 } from '../../../../src/util/matrixUtil';
 import { setLoadingPage } from '../../../../src/app/templates/client/Loading';
+import initMatrix from '../../../../src/client/initMatrix';
+
+import { useStore } from '../../../../src/app/hooks/useStore';
+
+const openRoom = (roomId) => {
+
+    const mx = initMatrix.matrixClient;
+    const room = mx.getRoom(roomId);
+
+    if (!room) return;
+    if (room.isSpaceRoom()) selectTab(roomId);
+
+    else {
+        selectRoomMode('room');
+        selectRoom(roomId);
+    }
+
+};
 
 // Models
 const valuesLoad = {
@@ -59,9 +77,34 @@ const valuesLoad = {
         title: 'room_name',
 
         // Data Button
-        getRoom: async (roomId) => {
+        getRoom: async (alias, mountStore) => {
 
-            console.log(roomId);
+            const mx = initMatrix.matrixClient;
+            setLoadingPage('Looking for address...');
+            let via;
+            if (alias.startsWith('#')) {
+                try {
+                    const aliasData = await mx.getRoomIdForAlias(alias);
+                    via = aliasData?.servers.slice(0, 3) || [];
+                    if (mountStore.getItem()) {
+                        setLoadingPage(`Joining ${alias}...`);
+                    }
+                } catch (err) {
+                    if (!mountStore.getItem()) return;
+                    setLoadingPage(false);
+                    alert(`Unable to find room/space with ${alias}. Either room/space is private or doesn't exist.`);
+                }
+            }
+            try {
+                const roomId = await join(alias, false, via);
+                if (!mountStore.getItem()) return;
+                openRoom(roomId);
+                setLoadingPage(false);
+            } catch {
+                if (!mountStore.getItem()) return;
+                setLoadingPage(false);
+                alert(`Unable to join ${alias}. Either room/space is private or doesn't exist.`);
+            }
 
         },
 
@@ -73,6 +116,7 @@ function ItemWelcome({ bot, type, item, itemsLength }) {
 
     // Refs
     const buttonRef = useRef(null);
+    const mountStore = useStore();
 
     // Effect
     useEffect(() => {
@@ -84,7 +128,7 @@ function ItemWelcome({ bot, type, item, itemsLength }) {
 
                 // Select tab and bot id
                 selectTab(valuesLoad[type].tab);
-                return valuesLoad[type].getRoom(button.attr('bot'));
+                return valuesLoad[type].getRoom(button.attr('bot'), mountStore);
 
             };
 

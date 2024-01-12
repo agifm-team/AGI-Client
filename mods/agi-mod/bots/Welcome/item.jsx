8b620e92ad52a4aae733f8cad953e23e3332ba7e
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-01-12 07:59:23 -0300

    tiny class css fix - welcome page

--- mods/agi-mod/bots/Welcome/item.jsx
+++ mods/agi-mod/bots/Welcome/item.jsx
@@ -140,7 +140,7 @@ function ItemWelcome({ bot, type, index, itemsLength, isGuest }) {
         ref={buttonRef}
         className={`citem col-md-2 col-sm-4 col-6${isGuest ? ' guest-mode' : ''}`} bot={typeof bot.id === 'string' && bot.id !== 'Coming soon!' ? bot.id : null}
     >
-        <div className={`border border-bg p-3 my-3 m${index > 0 ? index < itemsLength - 1 ? 'x-3' : 's-3' : 'e-3'}`}>
+        <div className={`border border-bg p-3 py-3 p${index > 0 ? index < itemsLength - 1 ? 'x-3' : 's-3' : 'e-3'}`}>
 
             <img className='img-fluid d-block avatar avatar-bg' draggable={false} alt='avatar' src={avatar} />
             <img className='img-fluid d-block avatar' draggable={false} alt='avatar' src={avatar} />

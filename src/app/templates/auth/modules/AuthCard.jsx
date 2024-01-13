50ed6dc40b969516a77f6ff94e58f84e30cd04d5
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-01-13 03:43:52 -0300

    preparing to fix login button

--- src/app/templates/auth/modules/AuthCard.jsx
+++ src/app/templates/auth/modules/AuthCard.jsx
@@ -7,7 +7,6 @@ import Register from './Register';
 global.authPublicData = {};
 function AuthCard() {
     const [hsConfig, setHsConfig] = useState(null);
-    const [type, setType] = useState('login');
 
     const handleHsChange = (info) => {
         setHsConfig(info);
@@ -22,28 +21,8 @@ function AuthCard() {
         </div>
 
         {hsConfig !== null && (
-            type === 'login'
-                ? <Login loginFlow={hsConfig.login.flows} baseUrl={hsConfig.baseUrl} />
-                : (
-                    <Register
-                        registerInfo={hsConfig.register}
-                        loginFlow={hsConfig.login.flows}
-                        baseUrl={hsConfig.baseUrl}
-                    />
-                )
+            <Login loginFlow={hsConfig.login.flows} baseUrl={hsConfig.baseUrl} />
         )}
-
-        {hsConfig !== null && (<>
-
-            { /* (type === 'login' && <a className="very-small" href="#!">Forgot password?</a>) */}
-
-            {/* <center>
-                <p className="mb-4 pb-lg-2 small" >{`${(type === 'login' ? 'Don\'t have' : 'Already have')} an account?`} <a href="#!" onClick={() => setType((type === 'login') ? 'register' : 'login')}>
-                    {type === 'login' ? 'Register here' : 'Login here'}
-                </a></p>
-            </center> */}
-
-        </>)}
 
     </>);
 }

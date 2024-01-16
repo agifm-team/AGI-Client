7f9243e6ace5bba90e8e7f1d6ff29ddeb62fd6e1
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-01-15 22:14:27 -0300

    welcome css migration

--- mods/agi-mod/bots/Welcome/index.jsx
+++ mods/agi-mod/bots/Welcome/index.jsx
@@ -178,7 +178,7 @@ function Welcome({ isGuest }) {
           <ChatRoomFrame
             hsUrl={isGuest && `https://matrix.${serverDomain}`}
             roomId={`#imagegen:${serverDomain}`}
-            className="border border-bg w-100"
+            className="border border-bg w-100 chatroom"
             style={{ height: 300 }}
             refreshTime={1}
           />
@@ -188,7 +188,7 @@ function Welcome({ isGuest }) {
           <ChatRoomFrame
             hsUrl={isGuest && `https://matrix.${serverDomain}`}
             roomId={`#previews:${serverDomain}`}
-            className="border border-bg w-100"
+            className="border border-bg w-100 chatroom"
             style={{ height: 300 }}
             refreshTime={1}
           />

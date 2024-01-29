2982f1c254d1a5341b6ccd41cc188a750a23e789
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-01-29 14:56:32 -0300

    item - agents button invite

--- mods/agi-mod/bots/PeopleSelector/Item.jsx
+++ mods/agi-mod/bots/PeopleSelector/Item.jsx
@@ -25,7 +25,7 @@ function PeopleSelector({ avatarSrc, name, user, peopleRole, }) {
         const button = $(buttonRef.current);
         const tinyButton = async (event) => {
 
-            const userId = $(event.currentTarget).attr('bot');
+            const userId = $(event.currentTarget).parent().parent().parent().parent().attr('bot');
             if (typeof userId === 'string') {
 
                 const roomId = getRoomInfo().roomTimeline.room.roomId;
@@ -74,10 +74,10 @@ function PeopleSelector({ avatarSrc, name, user, peopleRole, }) {
 
     });
 
-    return <div ref={buttonRef} bot={user?.userId || name} className="card agent-button noselect">
+    return <div bot={user?.userId || name} className="card agent-button noselect">
         <div className='text-start my-3 mx-4'><img src={avatarSrc} className="img-fluid avatar rounded-circle" draggable={false} height={100} width={100} alt="avatar" /></div>
         <div className="text-start card-body mt-0 pt-0">
-            <h5 className="card-title small text-bg">{name}</h5>
+            <h5 className="card-title small text-bg">{name}<div class="float-end"><button ref={buttonRef} className='btn btn-primary btn-sm my-1'>Invite</button></div></h5>
             <p className="card-text very-small text-bg-low">{peopleRole}</p>
         </div>
     </div>;

5f38d40dad8fc549b4622068b30078d2b5362cc6
seshubonam <seshu.bonam@gmail.com>
2024-02-14 21:04:43 +0530

    Update index.jsx

--- mods/agi-mod/bots/Welcome/index.jsx
+++ mods/agi-mod/bots/Welcome/index.jsx
@@ -227,24 +227,24 @@ function Welcome({ isGuest }) {
             <div class="carousel-item active">
               <img src="https://github.com/pixxels-team/Pixxels-App/blob/dev/public/img/homepage-slider/c2.jpg?raw=true" class="d-block w-100" draggable="false" alt="..." />
               <div class="carousel-caption">
-                <h5></h5>
-                <p></p>
+                <h5>Infinite models at for infinite creativity</h5>
+                <p>Fun and Productivity like never before</p>
               </div>
             </div>
 
             <div class="carousel-item">
               <img src="https://github.com/pixxels-team/Pixxels-App/blob/dev/public/img/homepage-slider/c1.jpg?raw=true" class="d-block w-100" draggable="false" alt="..." />
               <div class="carousel-caption">
-                <h5></h5>
-                <p></p>
+                <h5>Generative AI at your fingertips</h5>
+                <p>Create Seamless Variations tailored to your vision</p>
               </div>
             </div>
 
             <div class="carousel-item">
               <img src="https://github.com/pixxels-team/Pixxels-App/blob/dev/public/img/homepage-slider/c3.jpg?raw=true" class="d-block w-100" draggable="false" alt="..." />
               <div class="carousel-caption">
-                <h5></h5>
-                <p></p>
+                <h5>Collaborate and create custom Characters</h5>
+                <p>Instantly with no-code tools</p>
               </div>
             </div>
           </div>

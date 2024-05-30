1db78031aafabcca082dcf658fb04be2302a28d0
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-05-30 13:32:51 -0300

    loadThemes - fixed

--- mods/themeManager/index.js
+++ mods/themeManager/index.js
@@ -2,22 +2,26 @@ import './theme.scss';
 import tinyAPI from '../../src/util/mods';
 
 export default function startTheme() {
-  tinyAPI.on('loadThemes', (data, insertTheme, removeTheme, getThemeById, getThemeNameById) => {
-    const blackTheme = getThemeById('black-theme');
-    const darkTheme = getThemeById('dark-theme');
-    if (darkTheme) {
-      darkTheme.data = blackTheme.data;
-      darkTheme.text = blackTheme.text;
-      darkTheme.type = blackTheme.type;
-    }
+  tinyAPI.on(
+    'loadThemes',
+    (data, insertTheme, removeTheme, getThemeById, getThemeNameById, changeDefaultTypeSystem) => {
+      const blackTheme = getThemeById('black-theme');
+      const darkTheme = getThemeById('dark-theme');
+      if (darkTheme) {
+        darkTheme.data = blackTheme.data;
+        darkTheme.text = blackTheme.text;
+        darkTheme.type = blackTheme.type;
+      }
 
-    const darkThemeName = getThemeNameById('dark-theme');
-    if (darkThemeName) {
-      darkThemeName.data = blackTheme.data;
-      darkThemeName.text = blackTheme.text;
-      darkThemeName.type = blackTheme.type;
-    }
+      const darkThemeName = getThemeNameById('dark-theme');
+      if (darkThemeName) {
+        darkThemeName.data = blackTheme.data;
+        darkThemeName.text = blackTheme.text;
+        darkThemeName.type = blackTheme.type;
+      }
 
-    removeTheme('black-theme');
-  });
+      changeDefaultTypeSystem('dark', 'theme-type-dark-solid');
+      removeTheme('black-theme');
+    },
+  );
 }

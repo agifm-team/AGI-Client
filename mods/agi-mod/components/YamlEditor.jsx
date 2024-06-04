a24cf192f99381679090248780ebebc0a87b06bc
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-06-03 23:51:26 -0300

    codemirror update (still glitched)

--- mods/agi-mod/components/YamlEditor.jsx
+++ mods/agi-mod/components/YamlEditor.jsx
@@ -1,29 +1,48 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
+import CodeMirror from '@uiw/react-codemirror';
+import { yaml as yamlLang } from '@codemirror/lang-yaml';
+import settings from '@src/client/state/settings';
 import yaml from 'js-yaml';
 
-import CodeEditor from './CodeEditor';
-import 'codemirror/mode/yaml/yaml';
-import 'codemirror/addon/lint/yaml-lint';
-
-window.jsyaml = yaml;
-
 function YamlEditor({ value = '', isOpen = true }) {
+  const [tvalue, setValue] = useState(value);
+  const [theme, setTheme] = useState(null);
+
+  const onChange = useCallback((val, viewUpdate) => {
+    try {
+      const _yaml = yaml.loadAll(val);
+      console.log(_yaml);
+    } catch (e) {
+      console.error(e);
+    }
+    setValue(val);
+  }, []);
+
+  useEffect(() => {
+    const themeId = settings.getThemeType();
+    const systemTheme = settings.getSystemTheme();
+    const theTheme =
+      (systemTheme.enabled && systemTheme.isDark) ||
+      themeId === 'dark' ||
+      themeId === 'dark-solid' ||
+      themeId === 'dark2' ||
+      themeId === 'dark2-solid'
+        ? 'dark'
+        : 'light';
+
+    if (theTheme !== theme) setTheme(theTheme);
+  });
+
+  // https://uiwjs.github.io/react-codemirror/
   return (
-    <CodeEditor
-      isOpen={isOpen}
-      value={value}
-      mode="yaml"
-      onChange={(code) => {
-        // const content = code.getValue();
-        /* try {
-                            const _yaml = yaml.loadAll(content);
-                            console.log(_yaml);
-                        } catch (e) {
-                            console.error(e);
-                        } */
-      }}
+    <CodeMirror
+      value={tvalue}
+      theme={theme}
+      height="200px"
+      extensions={[yamlLang()]}
+      onChange={onChange}
     />
   );
 }

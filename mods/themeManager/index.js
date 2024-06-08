5e2329cb6cd1255389563b3c5f350c36751463d9
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-06-08 01:45:58 -0300

    theme fix

--- mods/themeManager/index.js
+++ mods/themeManager/index.js
@@ -5,22 +5,23 @@ export default function startTheme() {
   tinyAPI.on(
     'loadThemes',
     (data, insertTheme, removeTheme, getThemeById, getThemeNameById, changeDefaultTypeSystem) => {
-      const blackTheme = getThemeById('black-theme');
+      const blackTheme = getThemeById('black-colors-theme');
       const darkTheme = getThemeById('dark-theme');
       if (darkTheme) {
         darkTheme.data = blackTheme.data;
         darkTheme.type = blackTheme.type;
+        darkTheme.coloredIcons = blackTheme.coloredIcons;
       }
 
       const darkThemeName = getThemeNameById('dark-theme');
-      const blackThemeName = getThemeNameById('black-theme');
+      const blackThemeName = getThemeNameById('black-colors-theme');
       if (darkThemeName) {
         // darkThemeName.text = blackThemeName.text;
-        darkThemeName.text = 'Black';
+        darkThemeName.text = 'Black Colors';
       }
 
       changeDefaultTypeSystem('dark', 'theme-type-dark-solid');
-      removeTheme('black-theme');
+      removeTheme('black-colors-theme');
     },
   );
 }

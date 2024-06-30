39abdb108a794385fab1a3c0e3ce50bf9394e9f3
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-06-30 03:36:22 -0300

    script file migration

--- mods/agi-mod/bots/Welcome/item.jsx
+++ mods/agi-mod/bots/Welcome/item.jsx
@@ -1,30 +1,14 @@
 import React, { useEffect, useRef } from 'react';
 import { defaultAvatar } from '@src/app/atoms/avatar/defaultAvatar';
-import { selectRoom, selectRoomMode, selectTab } from '@src/client/action/navigation';
+import { selectTab } from '@src/client/action/navigation';
 import cons from '@src/client/state/cons';
 
-import * as roomActions from '@src/client/action/room';
-
-import { hasDMWith, hasDevices } from '@src/util/matrixUtil';
-import { setLoadingPage } from '@src/app/templates/client/Loading';
 import initMatrix from '@src/client/initMatrix';
-import { join } from '@src/client/action/room';
+import { joinAiBot, joinAiRoom, joinAiSpace } from './execute';
 // import { btModal } from '@src/util/tools';
 // import { ChatRoomjFrame } from '@src/app/embed/ChatRoom';
 
 // import { serverDomain } from '../../socket';
-
-const openRoom = (roomId) => {
-  const mx = initMatrix.matrixClient;
-  const room = mx.getRoom(roomId);
-
-  if (!room) return;
-  if (room.isSpaceRoom()) selectTab(roomId);
-  else {
-    selectRoomMode('room');
-    selectRoom(roomId);
-  }
-};
 
 // Models
 const valuesLoad = {
@@ -34,26 +18,7 @@ const valuesLoad = {
     tab: cons.tabs.DIRECTS,
 
     // Data Button
-    getRoom: async (userId) => {
-      // Check and open if user already have a DM with userId.
-      const dmRoomId = hasDMWith(userId);
-      if (dmRoomId) {
-        selectRoomMode('room');
-        selectRoom(dmRoomId);
-        return;
-      }
-
-      // Create new DM
-      try {
-        setLoadingPage();
-        const { room_id } = await roomActions.createDM(userId, await hasDevices(userId));
-        selectRoom(room_id);
-        setLoadingPage(false);
-      } catch (err) {
-        console.error(err);
-        alert(err.message);
-      }
-    },
+    getRoom: joinAiBot,
   },
 
   // Rooms
@@ -62,32 +27,7 @@ const valuesLoad = {
     tab: cons.tabs.HOME,
 
     // Data Button
-    getRoom: async (alias) => {
-      const mx = initMatrix.matrixClient;
-      setLoadingPage('Looking for address...');
-      let via;
-      if (alias.startsWith('#')) {
-        try {
-          const aliasData = await mx.getRoomIdForAlias(alias);
-          via = aliasData?.servers.slice(0, 3) || [];
-          setLoadingPage(`Joining ${alias}...`);
-        } catch (err) {
-          setLoadingPage(false);
-          console.error(err);
-          alert(
-            `Unable to find room/space with ${alias}. Either room/space is private or doesn't exist.`,
-          );
-        }
-      }
-      try {
-        const roomId = await join(alias, false, via);
-        openRoom(roomId);
-        setLoadingPage(false);
-      } catch {
-        setLoadingPage(false);
-        alert(`Unable to join ${alias}. Either room/space is private or doesn't exist.`);
-      }
-    },
+    getRoom: joinAiRoom,
   },
 
   // Spaces
@@ -96,32 +36,7 @@ const valuesLoad = {
     tab: cons.tabs.HOME,
 
     // Data Button
-    getRoom: async (alias) => {
-      const mx = initMatrix.matrixClient;
-      setLoadingPage('Looking for address...');
-      let via;
-      if (alias.startsWith('#')) {
-        try {
-          const aliasData = await mx.getRoomIdForAlias(alias);
-          via = aliasData?.servers.slice(0, 3) || [];
-          setLoadingPage(`Joining ${alias}...`);
-        } catch (err) {
-          setLoadingPage(false);
-          console.error(err);
-          alert(
-            `Unable to find room/space with ${alias}. Either room/space is private or doesn't exist.`,
-          );
-        }
-      }
-      try {
-        const roomId = await join(alias, false, via);
-        openRoom(roomId);
-        setLoadingPage(false);
-      } catch {
-        setLoadingPage(false);
-        alert(`Unable to join ${alias}. Either room/space is private or doesn't exist.`);
-      }
-    },
+    getRoom: joinAiSpace,
   },
 };
 

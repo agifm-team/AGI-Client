88d6f7f7e2c9ef99f2b0c3e25b28d07e796e94bf
Yasmin Seidel (JasminDreasond) <tiny@puddy.club>
2024-07-04 18:59:40 -0300

    AuthCard - prettier

--- src/app/templates/auth/modules/AuthCard.jsx
+++ src/app/templates/auth/modules/AuthCard.jsx
@@ -23,31 +23,34 @@ function AuthCard() {
     <>
       <Homeserver />
 
-      {objType(hsConfig, 'object') && objType(hsConfig.login, 'object') && Array.isArray(hsConfig.login.flows) && hsConfig.baseUrl && (
-        <nav className="navbar navbar-expand-lg bg-bg border-bottom border-bg fixed-top">
-          <div className="container-fluid">
-            <a className="navbar-brand text-bg-force">
-              {' '}
-              <img
-                src="./img/png/cinny.png"
-                alt="Logo"
-                width="24"
-                height="24"
-                class="d-inline-block align-text-top me-2"
-              />
-              {__ENV_APP__.INFO.name}
-            </a>
+      {objType(hsConfig, 'object') &&
+        objType(hsConfig.login, 'object') &&
+        Array.isArray(hsConfig.login.flows) &&
+        hsConfig.baseUrl && (
+          <nav className="navbar navbar-expand-lg bg-bg border-bottom border-bg fixed-top">
+            <div className="container-fluid">
+              <a className="navbar-brand text-bg-force">
+                {' '}
+                <img
+                  src="./img/png/cinny.png"
+                  alt="Logo"
+                  width="24"
+                  height="24"
+                  class="d-inline-block align-text-top me-2"
+                />
+                {__ENV_APP__.INFO.name}
+              </a>
 
-            <div className="navbar-nav small">
-              <Login
-                hsConfig={hsConfig}
-                loginFlow={hsConfig.login.flows}
-                baseUrl={hsConfig.baseUrl}
-              />
+              <div className="navbar-nav small">
+                <Login
+                  hsConfig={hsConfig}
+                  loginFlow={hsConfig.login.flows}
+                  baseUrl={hsConfig.baseUrl}
+                />
+              </div>
             </div>
-          </div>
-        </nav>
-      )}
+          </nav>
+        )}
     </>
   );
 }
